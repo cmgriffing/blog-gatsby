74b65f91869314427baa2e72ced108aac8f7db3f
Chris Griffing <cmgriffing@gmail.com>
2021-07-27 17:00:19 -0700

    fix: add missing remark images repo

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,6 +66,15 @@ module.exports = {
       options: {
         plugins: [
           {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 590,
+            },
+          },
+          {
             resolve: `gatsby-remark-prismjs`,
             options: {},
           },
